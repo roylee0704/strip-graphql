553a2468cf0a5715bfbaad89aca3bf3cdaca70d5
roylee0704 <roylee0704@gmail.com>
2019-11-15 11:28:23 +0700

    index: Add roll example

--- index.js
+++ index.js
@@ -2,37 +2,46 @@ var { graphql, buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
-  type Query {
-    hello: String
-    human: Human
-    homans: Human
+  type RandomDice {
+    numSides: Int!
+    rollOnce: Int!
+    roll(numRolls: Int!): [Int]
   }
 
-  type Human {
-      name: String
+  type Query {
+      die(numSides: Int): RandomDice
   }
 `);
 
+
+// create a es6-class that has 'props' and 'methods'(aka props in js) as graphql
+class Dice {
+    constructor(numSides) {
+        this.numSides = numSides;
+    }
+
+    // auto-resolve
+    rollOnce() {
+        return 1 + Math.floor(Math.random() * this.numSides);
+    }
+
+    roll({ numRolls }) {
+        var output = [];
+        for (var i = 0; i < numRolls; i++) {
+            output.push(this.rollOnce());
+        }
+        return output;
+    }
+}
+
 // The root provides a resolver function for each API endpoint
 var root = {
-    hello: () => {
-        return 'Hello world!';
-    },
-    human: () => {
-        return { name: 'Roy' };
-    },
-    homans: () => {
-        return { name: 'Roy' };
+    die: ({ numSides }) => {
+        return new Dice(numSides);
     },
 };
 
 // Run the GraphQL query '{ hello }' and print out the response
-graphql(schema, '{ human { name } }', root).then((response) => {
-    console.log('humans', response);
-});
-
-
-
-graphql(schema, '{ homans { name } }', root).then((response) => {
-    console.log('homans:', response);
+graphql(schema, '{ die(numSides: 6) { rollOnce, roll(numRolls: 2) } }', root).then((response) => {
+    console.log(JSON.stringify(response));
 });
